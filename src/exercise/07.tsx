0cda0211f3eb244a4c70aea6df152ee89315ffcc
Shourya Bansal <shouryabansal2012@gmail.com>
2021-04-01 17:51:23 -0400

    ✅ Begin Excersise 7

--- src/exercise/07.tsx
+++ src/exercise/07.tsx
@@ -3,30 +3,29 @@
 
 import * as React from 'react'
 
+interface FormElements extends HTMLFormControlsCollection {
+  usernameInput: HTMLInputElement
+}
+
+interface UsernameFormElement extends HTMLFormElement {
+  readonly elements: FormElements
+}
+
 function UsernameForm({
   onSubmitUsername,
 }: {
   onSubmitUsername: (username: string) => void
 }) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  function handleSubmit(event: React.SyntheticEvent<UsernameFormElement>) {
+    event.preventDefault()
+    onSubmitUsername(event.currentTarget.elements.usernameInput.value)
+  }
 
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
